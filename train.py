3edba6be1428dc2b235582dcbd986f467cf2bd14
Emil Aby <emilaby123@gmail.com>
2026-07-26 19:00:56 +0100

    RandomForestRegressor trained on data, has avg rmse of £5095

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import TargetEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import KFold, cross_val_score
+
 
 
 df = pd.read_csv("car_listings_cleaned.csv")
@@ -16,11 +20,25 @@ X_train, X_test, y_train, y_test = train_test_split(
 encoder = ColumnTransformer(
     transformers=[
         ("one_hot_encoder", OneHotEncoder(handle_unknown="ignore"), ["make", "body_type", "transmission", "fuel_type"]),
-        ("target_encoder", TargetEncoder(cv=5, smooth="auto", random_state=42), ["model", "variant"])
+        ("target_encoder", TargetEncoder(cv=3, smooth="auto", random_state=42, target_type="continuous"), ["model", "variant"])
     ],
     remainder="passthrough"
 )
 
-X_train_enc = encoder.fit_transform(X_train, y_train)
-X_test_enc = encoder.transform(X_test)
+pipeline = Pipeline([
+    ("preprocessor", encoder),
+    ("model", RandomForestRegressor(random_state=42, n_jobs=-1, max_depth=20, n_estimators=50, verbose=1))
+])
 
+
+kf_validation = KFold(n_splits=3, shuffle=True, random_state=42)
+
+scores = cross_val_score(
+    pipeline,
+    X_train,
+    y_train,
+    scoring="neg_root_mean_squared_error",
+    cv=kf_validation
+) 
+
+print(scores)
