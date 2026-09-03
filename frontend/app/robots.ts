675f821dd413796de04819df1d422431f552c6b5
Emil Aby <emilaby123@gmail.com>
2026-09-03 15:02:23 +0100

    Removed trailing / from baseUrl.

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://www.carvalai.co.uk/"
+  const baseUrl = "https://www.carvalai.co.uk"
 
   return {
     rules: [
